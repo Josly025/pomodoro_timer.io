3052d22d4df817876d04f7b9cfe54595abd5abc7
Grantjos12 <grantjoslyn@gmail.com>
2021-01-08 17:17:36 -0600

    timer working

--- index.js
+++ index.js
@@ -4,26 +4,65 @@ const pauseBtn = document.getElementById("pause");
 
 //one class for all events and arguments from DOM
 class Timer {
-  constructor(timeInput, startBtn, pauseBtn) {
+  constructor(timeInput, startBtn, pauseBtn, callbacks) {
     this.timeInput = timeInput;
     this.startBtn = startBtn;
     this.pauseBtn = pauseBtn;
 
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+    }
+
     this.startBtn.addEventListener("click", this.startTimer);
     this.pauseBtn.addEventListener("click", this.pauseTimer);
   }
+
   startTimer = () => {
-    console.log("timer is starting");
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.currentTime();
+
     this.interval = setInterval(this.currentTime, 1000);
   };
+
   pauseTimer = () => {
     clearInterval(this.interval);
   };
+
   currentTime = () => {
-    console.log("tick");
-    const timeRemaing = parseFloat(this.timeInput.value);
-    this.timeInput = timeRemaing - 1;
+    const timeRemaining = parseFloat(this.timeInput.value);
+    if (this.timeRemaining <= 0) {
+      this.pauseTimer();
+      if (this.onComplete) {
+        this.onComplete;
+      }
+    } else {
+      this.timeRemaining = timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
   };
+
+  //getter function -- invoked on line 24
+  get timeRemaining() {
+    return parseFloat(this.timeInput.value);
+  }
+
+  set timeRemaining(time) {
+    return (this.timeInput.value = time);
+  }
 }
 
-const timer = new Timer(timeInput, startBtn, pauseBtn);
+const timer = new Timer(timeInput, startBtn, pauseBtn, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer just ticked down");
+  },
+  onComplete() {
+    console.log("Timer is completed");
+  },
+});
